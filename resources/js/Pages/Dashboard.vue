9fc5d31b088e0672818ec91402316f1e6483c11c
Ibrahim Fawaz <ibrahimkfawaz@gmail.com>
2024-01-11 23:25:10 +0100

    feat: add table to show prices and evolution

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,53 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+const stockPrices = ref([]);
+
+onMounted(async () => {
+    try {
+        const response = await fetch('api/stocks');
+        const data = await response.json();
+        stockPrices.value = data.data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <div>
+        <Head title="Dashboard" />
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-        </template>
+        <AuthenticatedLayout>
+            <template #header>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
+
+            <div class="py-4 flex items-center justify-center">
+                <table class="min-w-56 bg-white border border-gray-300">
+                    <thead>
+                    <tr>
+                        <th class="border-b">Ticker</th>
+                        <th class="border-b">Evolution</th>
+                        <th class="border-b">Trend</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="price in stockPrices" :key="price.symbol">
+                        <td class="border-b">{{ price.ticker }}</td>
+                        <td class="border-b">{{ price.price_evolution }}</td>
+                        <td class="py-2 px-4 border-b">
+                          <span :class="{ 'text-green-500': price.price_evolution >= 0, 'text-red-500': price.price_evolution < 0 }">
+                            {{ price.price_evolution > 0 ? '▲' : price.price_evolution < 0 ? '▼' : '-' }}
+                          </span>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
-        </div>
-    </AuthenticatedLayout>
+        </AuthenticatedLayout>
+    </div>
 </template>
