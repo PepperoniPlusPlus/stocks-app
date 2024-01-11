9b0b69395dc5feb8cd5d6987982a8beed5c465b5
Ibrahim Fawaz <ibrahimkfawaz@gmail.com>
2024-01-11 21:43:41 +0100

    feat: add attributes and relationships

--- app/Models/StockPrice.php
+++ app/Models/StockPrice.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $stock_id
+ * @property float $price
+ */
 class StockPrice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'stock_id',
+        'price',
+    ];
+
+    public function stock(): BelongsTo
+    {
+        return $this->belongsTo(Stock::class);
+    }
 }
